f270ecdb576456fb75d21edcf36e556e3806ea63
bshock84 <bshock84@yahoo.com>
2019-02-06 15:06:31 -0600

    Initial Commit

---
+++ Soaper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Soaper
+//
+//  Created by Ben Shockley on 2/6/19.
+//  Copyright © 2019 Ben Shockley. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
